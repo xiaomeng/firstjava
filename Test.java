305eb93cbbba4a60a78d8047169bf0cf72241ef5
xiaomeng <xiaomeng@citruslane.com>
2013-09-13 14:35:12 -0700

    commit the change in the branch on Test.java

--- Test.java
+++ Test.java
@@ -5,6 +5,12 @@ public class Test {
 	    System.out.println("this is a test from my work!");
 
             System.out.println("print out a test line 2");
+	
+
+	    System.out.println("this is a check from b1 in cloned code base");
+	
 	}
 
+
+	
 }
