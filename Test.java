df0a362fa0f55021d06e6b87cd8d953f1b0fb39d
xiaomeng <xiaomeng@citruslane.com>
2013-09-13 10:36:06 -0700

    check in from my cloned dir code base

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ public class Test {
 	public static void main( String[] args ){
 	
 	    System.out.println("this is a test from my work!");
+
+            System.out.println("print out a test line 2");
 	}
 
 }
